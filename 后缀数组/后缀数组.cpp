7256491bd8563ad9fdeac6c59d19449d82df0c8f
lingzerol <467264565@qq.com>
2017-10-06 23:03:47 +0800

    后缀数组
    稍作修改

--- 后缀数组/后缀数组.cpp
+++ 后缀数组/后缀数组.cpp
@@ -7,26 +7,26 @@ using namespace std;
 const int MAXN = 100005;
 
 char ch[MAXN], All[MAXN];
-int SA[MAXN], rank[MAXN], Height[MAXN], tax[MAXN], tp[MAXN], a[MAXN], n, m; 
+int SA[MAXN], rank1[MAXN], Height[MAXN], tax[MAXN], tp[MAXN], a[MAXN], n, m; 
 char str[MAXN];
 //rank[i] 第i个后缀的排名; SA[i] 排名为i的后缀位置; Height[i] 排名为i的后缀与排名为(i-1)的后缀的LCP
 //tax[i] 计数排序辅助数组; tp[i] rank的辅助数组(计数排序中的第二关键字),与SA意义一样。
 //a为原串
-void RSort() {
+void RSort(int *rank1,int *tp) {
     //rank第一关键字,tp第二关键字。
     for (int i = 0; i <= m; i ++) tax[i] = 0;
-    for (int i = 1; i <= n; i ++) tax[rank[tp[i]]] ++;
+    for (int i = 1; i <= n; i ++) tax[rank1[tp[i]]] ++;
     for (int i = 1; i <= m; i ++) tax[i] += tax[i-1];
-    for (int i = n; i >= 1; i --) SA[tax[rank[tp[i]]] --] = tp[i]; //确保满足第一关键字的同时，再满足第二关键字的要求
+    for (int i = n; i >= 1; i --) SA[tax[rank1[tp[i]]] --] = tp[i]; //确保满足第一关键字的同时，再满足第二关键字的要求
 } //计数排序,把新的二元组排序。
 
 int cmp(int *f, int x, int y, int w) { return f[x] == f[y] && f[x + w] == f[y + w]; } 
 //通过二元组两个下标的比较，确定两个子串是否相同
 
-void Suffix() {
+void Suffix(int *rank1,int *tp) {
     //SA
-    for (int i = 1; i <= n; i ++) rank[i] = a[i], tp[i] = i;
-    m = 127 ,RSort(); //一开始是以单个字符为单位，所以(m = 127)
+    for (int i = 1; i <= n; i ++) rank1[i] = a[i], tp[i] = i;
+    m = 127 ,RSort(rank1,tp); //一开始是以单个字符为单位，所以(m = 127)
 
     for (int w = 1, p = 1, i; p < n; w += w, m = p) { //把子串长度翻倍,更新rank
 
@@ -36,16 +36,16 @@ void Suffix() {
         for (i = 1; i <= n; i ++) if (SA[i] > w) tp[++ p] = SA[i] - w;
 
         //更新SA值,并用tp暂时存下上一轮的rank(用于cmp比较)
-        RSort(), swap(rank, tp), rank[SA[1]] = p = 1;
+        RSort(rank1,tp), swap(rank1, tp), rank1[SA[1]] = p = 1;
 
         //用已经完成的SA来更新与它互逆的rank,并离散rank
-        for (i = 2; i <= n; i ++) rank[SA[i]] = cmp(tp, SA[i], SA[i - 1], w) ? p : ++ p;
+        for (i = 2; i <= n; i ++) rank1[SA[i]] = cmp(tp, SA[i], SA[i - 1], w) ? p : ++ p;
     }
     //离散：把相等的字符串的rank设为相同。
     //LCP
     int j, k = 0;
-    for(int i = 1; i <= n; Height[rank[i ++]] = k) 
-        for( k = k ? k - 1 : k, j = SA[rank[i] - 1]; a[i + k] == a[j + k]; ++ k);
+    for(int i = 1; i <= n; Height[rank1[i ++]] = k) 
+        for( k = k ? k - 1 : k, j = SA[rank1[i] - 1]; a[i + k] == a[j + k]; ++ k);
     //这个知道原理后就比较好理解程序
 }
 
